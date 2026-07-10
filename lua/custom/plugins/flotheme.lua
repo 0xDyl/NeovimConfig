b63372a62091ca91bbc68a6805faf8b2bd65aa0f
Dylan Faulkner <dylanfaulkner616@gmail.com>
2026-07-10 19:39:28 +0100

    Created some personal plugins

--- lua/custom/plugins/flotheme.lua
+++ lua/custom/plugins/flotheme.lua
@@ -1,35 +1,3 @@
-local function create_floating_window()
-  local buf = vim.api.nvim_create_buf(true, false)
-
-  local screen_width = vim.o.columns
-  local screen_height = vim.o.lines
-
-  local win_width = math.floor(screen_width * 0.10)
-  local win_height = math.floor(screen_height * 0.10)
-
-  local row = math.floor((screen_height - win_height) / 2)
-  local col = math.floor((screen_width - win_width) / 2)
-
-  local win_opts = {
-    relative = 'editor',
-    width = win_width,
-    height = win_height,
-    row = row,
-    col = col,
-    style = 'minimal',
-    border = 'rounded',
-  }
-
-  -- Sets the close window keybind
-  vim.keymap.set('n', 'q', function() vim.api.nvim_buf_delete(buf, { force = true }) end)
-
-  local win = vim.api.nvim_open_win(buf, true, win_opts)
-
-  return buf, win
-end
-
--- Above is floating window logic only
-
 local function change_theme(theme)
   vim.cmd('colorscheme ' .. theme)
   print('Current theme: ' .. theme)
@@ -52,6 +20,43 @@ local themes = {
   'kanagawa',
   'catppuccin-mocha',
 }
+
+local function create_floating_window()
+  local buf = vim.api.nvim_create_buf(true, false)
+  vim.api.nvim_buf_set_name(buf, 'Theme Switcher')
+
+  local screen_width = vim.o.columns
+  local screen_height = vim.o.lines
+
+  local win_width = math.floor(screen_width * 0.10)
+  local win_height = #themes
+
+  local row = math.floor((screen_height - win_height) / 2)
+  local col = math.floor((screen_width - win_width) / 2)
+
+  local win_opts = {
+    relative = 'editor',
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col,
+    style = 'minimal',
+    border = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+  }
+
+  local win = vim.api.nvim_open_win(buf, true, win_opts)
+
+  -- "wipe" means just delete the buffer when it is hidden off screen
+  vim.api.nvim_set_option_value('bufhidden', 'wipe', { buf = buf })
+
+  -- Sets the close window keybind
+  vim.keymap.set('n', 'q', function()
+    vim.api.nvim_win_close(win, true)
+    -- vim.api.nvim_buf_delete(buf, { force = true }) -- Was using before the above "wipe"
+  end)
+
+  return buf, win
+end
 
 -- Will set default every startup
 local default = 'melange'
---
+++ lua/custom/plugins/todo.lua
@@ -0,0 +1,79 @@
+local function create_floating_window()
+  local buf = vim.api.nvim_create_buf(true, false)
+  vim.api.nvim_buf_set_name(buf, 'Todo')
+
+  local screen_width = vim.o.columns
+  local screen_height = vim.o.lines
+
+  local win_width = math.floor(screen_width * 0.45)
+  local win_height = math.floor(screen_height * 0.45)
+
+  local row = math.floor((screen_height - win_height) / 2)
+  local col = math.floor((screen_width - win_width) / 2)
+
+  local win_opts = {
+    relative = 'editor',
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col,
+    style = 'minimal',
+    border = 'rounded',
+  }
+
+  -- Sets the close window keybind
+  vim.keymap.set('n', 'q', function() vim.api.nvim_buf_delete(buf, { force = true }) end)
+
+  local win = vim.api.nvim_open_win(buf, true, win_opts)
+
+  return buf, win
+end
+
+local filepath = vim.fn.expand '~/.config/helper-files/todo.txt'
+
+vim.keymap.set('n', '<leader>td', function()
+  local buf, win = create_floating_window()
+  local file = io.open(filepath, 'r')
+
+  -- Read file
+  local lines = {}
+
+  if vim.fn.filereadable(filepath) == 1 then
+    for line in io.lines(filepath) do
+      table.insert(lines, line)
+    end
+  else
+    lines = { 'Nothing found' }
+  end
+
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
+  vim.api.nvim_create_autocmd({ 'BufDelete', 'BufWriteCmd' }, {
+    buffer = buf,
+    callback = function()
+      local current_lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+      local file = io.open(filepath, 'w')
+      if file then
+        for _, line in ipairs(current_lines) do
+          file:write(line .. '\n')
+        end
+        file:close()
+      end
+    end,
+  })
+
+  -- Will make checkboxes
+  vim.keymap.set('n', '<CR>', function()
+    local line = vim.api.nvim_get_current_line()
+    local line_i = vim.api.nvim_win_get_cursor(0)[1]
+
+    if string.find(line, '%[ %]') then
+      local newline = string.gsub(line, '%[ %]', '%[x%]')
+      vim.api.nvim_buf_set_lines(0, line_i - 1, line_i, false, { newline })
+    elseif string.find(line, '%[x%]') then
+      local newline = string.gsub(line, '%[x%]', '%[ %]')
+      vim.api.nvim_buf_set_lines(0, line_i - 1, line_i, false, { newline })
+    end
+  end, { buf = buf })
+end, { desc = 'Opens Todo-list' })
