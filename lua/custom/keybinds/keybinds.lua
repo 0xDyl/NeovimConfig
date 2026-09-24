abc84cddcbcd0572ea8bb4455b76e1e7a7a3c7dd
Dylan Faulkner <dylanfaulkner616@gmail.com>
2026-09-24 05:52:49 +0100

    Added the window creating keybinds

--- lua/custom/keybinds/keybinds.lua
+++ lua/custom/keybinds/keybinds.lua
@@ -3,3 +3,7 @@ vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open Oil Buffer' })
 
 -- Delete current buffer
 vim.keymap.set('n', '<leader>bd', function() vim.api.nvim_buf_delete(0, { force = false }) end, { desc = 'Deletes the current buffer' })
+
+-- Opens new windows; both vertical and horizontal
+vim.keymap.set('n', '<leader>-', '<cmd>split<CR>', { desc = 'Open a new horizontal window' })
+vim.keymap.set('n', '<leader>|', '<cmd>vsplit<CR>', { desc = 'Open a new vertical window' })
